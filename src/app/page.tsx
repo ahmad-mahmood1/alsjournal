d772b5631a23761f575ee7e9dac1cdbaf0ac723e
ahmad-mahmood1 <ahmad.mahmood.a90@gmail.com>
2024-03-24 23:05:52 +0500

    remove db pull

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,8 @@
-import Link from "next/link";
-import { db } from "../server/db";
-import { jioPosts } from "../server/db/schema";
-import { eq } from "drizzle-orm";
 
 export default async function HomePage() {
-  const post = await db.select().from(jioPosts).where(eq(jioPosts.id,483))
-  console.log(post)
   return (
-    <main className="min-h-screen bg-white">
-      <div key={post?.[0]?.id} dangerouslySetInnerHTML={{__html:post?.[0]?.postContent ?? <></>}}>
-      </div>  
-      
+    <main className="min-h-screen bg-white flex justify-center items-center">
+      ALS Journal
     </main>
   );
 }
